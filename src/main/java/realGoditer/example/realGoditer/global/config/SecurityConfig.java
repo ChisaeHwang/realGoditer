ad1f5136dd0983b0a126f0f79367358169a78195
ChisaeHwang <cwh73090@naver.com>
2023-10-10 23:52:25 +0900

    spa security

--- src/main/java/realGoditer/example/realGoditer/global/config/SecurityConfig.java
+++ src/main/java/realGoditer/example/realGoditer/global/config/SecurityConfig.java
@@ -1,25 +1,47 @@
 package realGoditer.example.realGoditer.global.config;
 
-import lombok.RequiredArgsConstructor;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
+import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.client.RestTemplate;
 import realGoditer.example.realGoditer.domain.member.service.CustomOAuth2UserService;
+import realGoditer.example.realGoditer.global.config.jwt.JwtAuthenticationFilter;
+import realGoditer.example.realGoditer.global.config.jwt.JwtTokenProvider;
+import realGoditer.example.realGoditer.global.config.oauth.HttpCookieOAuth2AuthorizationRequestRepository;
+import realGoditer.example.realGoditer.global.config.oauth.OAuth2AuthenticationFailureHandler;
+import realGoditer.example.realGoditer.global.config.oauth.OAuth2AuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     private final CustomOAuth2UserService customOAuth2UserService;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
+    private final OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler;
 
-    public SecurityConfig(CustomOAuth2UserService customOAuth2UserService) {
+    public SecurityConfig(CustomOAuth2UserService customOAuth2UserService, JwtTokenProvider jwtTokenProvider, OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler, OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler) {
         this.customOAuth2UserService = customOAuth2UserService;
+        this.jwtTokenProvider = jwtTokenProvider;
+        this.oAuth2AuthenticationSuccessHandler = oAuth2AuthenticationSuccessHandler;
+        this.oAuth2AuthenticationFailureHandler = oAuth2AuthenticationFailureHandler;
     }
+
+    @Bean
+    public HttpCookieOAuth2AuthorizationRequestRepository cookieOAuth2AuthorizationRequestRepository() {
+        return new HttpCookieOAuth2AuthorizationRequestRepository();
+    }
+
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
@@ -31,18 +53,36 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .formLogin().disable()
-                .httpBasic().disable()
-                .authorizeRequests()
-                .requestMatchers ("/api/**", "/login/**", "/oauth2/**").permitAll ()
-                .and()
-                .oauth2Login()
-                .userInfoEndpoint().userService(customOAuth2UserService);
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .formLogin(formLogin -> formLogin.disable())
+                .httpBasic(httpBasic -> httpBasic.disable())
+                .authorizeRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/api/**", "/login/**", "/oauth2/**").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .oauth2Login(oauth2Login -> {
+                    oauth2Login
+                            .authorizationEndpoint()
+                            .baseUri("/oauth2/authorize")
+                            .authorizationRequestRepository(cookieOAuth2AuthorizationRequestRepository());
+                    oauth2Login
+                            .redirectionEndpoint()
+                            .baseUri("/login/oauth2/code/**");
+                    oauth2Login
+                            .userInfoEndpoint()
+                            .userService(customOAuth2UserService);
+                    oauth2Login
+                            .successHandler(oAuth2AuthenticationSuccessHandler)
+                            .failureHandler(oAuth2AuthenticationFailureHandler);
+                })
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
+
+
 }
