ccf40e53d55ddb4eed01b076791e7fc4128ec915
ChisaeHwang <cwh73090@naver.com>
2024-01-31 00:50:27 +0900

    remove cached

--- src/main/java/realGoditer/example/realGoditer/infra/seeding/DataSeeder.java
+++ src/main/java/realGoditer/example/realGoditer/infra/seeding/DataSeeder.java
@@ -40,9 +40,9 @@ public class DataSeeder {
 
 //        // 시드 유저 생성
 //        List<User> users = List.of(
-//                User.of("조재홍", "userone@example.com", "password1", 0L, Role.EDITOR, "provider1"),
-//                User.of("느대", "usertwo@example.com", "password2", 0L, Role.THUMBNAILER, "provider2"),
-//                User.of("치피치피", "userthree@example.com", "password3", 0L, Role.EDITOR, "provider3")
+//                User.of("편집자A", "userone@example.com", "password1", 0L, Role.EDITOR, "provider1"),
+//                User.of("디자이너A", "usertwo@example.com", "password2", 0L, Role.THUMBNAILER, "provider2"),
+//                User.of("편집자B", "userthree@example.com", "password3", 0L, Role.EDITOR, "provider3")
 //        );
 //
 //        users.forEach(userRepository::save);
@@ -52,9 +52,9 @@ public class DataSeeder {
 //        taskListRepository.save(taskList);
 //
 //
-//        Task task1 = Task.from("비디오 편집", 10.0, 100.0, 10000, LocalDate.of(2023, 12, 1), LocalDate.of(2023, 12, 5), "조재홍", taskList);
-//        Task task2 = Task.from("썸네일 디자인", 2.0, 50.0, 10000, LocalDate.of(2023, 12, 3), LocalDate.of(2023, 12, 4), "느대", taskList);
-//        Task task3 = Task.from("영상 자막 작업", 5.0, 75.0, 10000, LocalDate.of(2023, 12, 2), LocalDate.of(2023, 12, 7), "치피치피", taskList);
+//        Task task1 = Task.from("비디오 편집", 10.0, 100.0, 10000, LocalDate.of(2023, 12, 1), LocalDate.of(2023, 12, 5), "편집자A", taskList);
+//        Task task2 = Task.from("썸네일 디자인", 2.0, 50.0, 10000, LocalDate.of(2023, 12, 3), LocalDate.of(2023, 12, 4), "디자이너A", taskList);
+//        Task task3 = Task.from("영상 자막 작업", 5.0, 75.0, 10000, LocalDate.of(2023, 12, 2), LocalDate.of(2023, 12, 7), "편집자B", taskList);
 //
 //        task3.setStatus(TaskStatus.COMPLETED);
 //
